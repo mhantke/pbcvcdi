a1e3c27b908b391bc4dd864703935841fc731d8c
hgsur-a@protein.osaka-u.ac.jp <hgsur.a@gmail.com>
2015-03-26 18:39:35 +0900

    Now full reconstructions are working.

--- rec.py
+++ rec.py
@@ -1,16 +1,16 @@
 import spimage,numpy
 import h5py
-from matplotlib.pyplot import figure
+from matplotlib.pyplot import figure,imsave
 
 def full_reconstruction(filename,index_number,S,N_rec=100,downsampling=2):
     I,M = load_pattern(filename,downsampling)
-    O_N = reconstruct(I,M,S,N_rec,"./rec_%s.cxi" % index_number)
+    O_N = reconstruct(I,M,S,N_rec,"./%s_rec.cxi" % index_number)
     ferr = O_N["scores_final"]["fourier_error"]
     rs = O_N["real_space_final"]
     sup = O_N["support_final"]
     sel = ferr[:] < S["fourier_error_max"]
     p = prtf(rs[sel,:,:],sup[sel,:,:],"./prtf_%s.h5" % index_number)
-    plot_prtf(p,"./prtf_%s.png" % (index_number),downsampling)
+    plot_prtf(p,"./%s" % (index_number),downsampling)
 
 def load_pattern(filename,downsampling=2):
     img = spimage.sp_image_read(filename,0)
@@ -25,7 +25,6 @@ def reconstruct(I,M,S,N=None,filename=None):
     R.set_intensities(I,shifted=False)
     R.set_mask(M,shifted=False)
     R.set_number_of_iterations(4000)
-    R.set_number_of_outputs(100)
     R.set_initial_support(radius=10)
     R.set_phasing_algorithm("raar",beta_init=0.9,beta_final=0.9,constraints=["enforce_positivity"],number_of_iterations=2000)
     R.append_phasing_algorithm("raar",beta_init=0.9,beta_final=0.9,constraints=["enforce_positivity"],number_of_iterations=1000)
@@ -34,8 +33,10 @@ def reconstruct(I,M,S,N=None,filename=None):
     R.append_support_algorithm("area",blur_init=6.,blur_final=1.,area_init=S["area_init_2nd"],area_final=S["area_final_2nd"],update_period=100,number_of_iterations=1000,center_image=False)
     R.append_support_algorithm("static",number_of_iterations=1000,center_image=False)
     if N is None:
+        R.set_number_of_outputs(100)
         O = R.reconstruct()
     else:
+        R.set_number_of_outputs(3)
         O = R.reconstruct_loop(N)
         if filename is not None:
             print filename
@@ -53,7 +54,7 @@ def prtf(real_space,support,filename=None):
                 f[k] = v
     return prtf
 
-def plot_prtf(prtf,filename,downsampling):
+def plot_prtf(prtf,filename_root,downsampling):
     P = spimage.downsample(prtf["prtf"],downsampling)
     Pr = spimage.radial_mean(P,cx=P.shape[1]/2,cy=P.shape[0]/2)
     from scipy.ndimage.filters import gaussian_filter
@@ -64,14 +65,13 @@ def plot_prtf(prtf,filename,downsampling):
     detector_distance = 0.73
     wavelength = 1.1E-9
     res = spimage.detector_pixel_to_resolution_element(i_pixel=x,pixel_size=pixel_size,detector_distance=detector_distance,wavelength=wavelength)
-    fig = figure(figsize=(4,2))
+    fig = figure(figsize=(5,3))
     q = 1/res/2.*1E-9
     if (Prs<1/numpy.e).sum() > 0:
         q_res = (q[Prs<1/numpy.e])[0]
     else:
         q_res = q[-1]
     ax = fig.add_subplot(111)
-    print Pr
     ax.plot(q,Pr)
     ax.plot(q,Prs)
     ax.axhline(1/numpy.e,ls="--",color="black")
@@ -81,4 +81,11 @@ def plot_prtf(prtf,filename,downsampling):
     ax.set_ylabel("PRTF")
     ax.set_ylim(0,1)
     #title("Fourier_error_max:%s" % (S["fourier_error_max"]))
-    fig.savefig(filename)
+    fig.savefig(filename_root+"_prtf.png",dpi=300)
+    I = abs(prtf["super_image"])
+    imsave(filename_root+"_super.png",I)
+    from python_tools import imgtools,gentools,colormaptools
+    Ic = imgtools.crop(I,40,"center_of_mass")
+    imsave(filename_root+"_super_cropped.png",Ic)
+    Icu = imgtools.upsample(Ic,20)
+    imsave(filename_root+"_super_cropped_upsampled.png",Icu,cmap=colormaptools.cmaps["jet_lightbg2"])
